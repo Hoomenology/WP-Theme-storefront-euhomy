dd76c848f18fdf6e7545d4110b05dea53cbb7fe1
xoinstein <xoinstein@gmail.com>
2020-10-14 17:04:55 +0800

    add shop页过滤掉 bundled-products 商品

--- functions.php
+++ functions.php
@@ -17,3 +17,27 @@ function mimvp_file_is_displayable_image($result, $path) {
     return $result;
 }
 add_filter( 'file_is_displayable_image', 'mimvp_file_is_displayable_image', 10, 2 );
+
+/**
+ * shop页过滤掉 bundled-products 商品
+ */
+function woo_custom_pre_get_posts_query( $q ) {
+ 
+    if ( ! $q->is_main_query() ) return;
+    if ( ! $q->is_post_type_archive() ) return;
+   
+    if ( ! is_admin() && is_shop() ) {
+   
+      $q->set( 'tax_query', array(array(
+        'taxonomy' => 'product_cat',
+        'field' => 'slug',
+        'terms' => array( 'bundled-products' ), // Don't display products in the shoes category on the shop page
+        'operator' => 'NOT IN'
+      )));
+   
+    }
+   
+    remove_action( 'pre_get_posts', 'custom_pre_get_posts_query' );
+   
+  }
+  add_action( 'pre_get_posts', 'woo_custom_pre_get_posts_query' );
